ae49d216137cec9ba266850ea418b5a602cbf709
open-cover <admin@opencover.net>
2026-07-07 15:42:50 -0600

    version 1.1.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_paths(directory):
 
 setup(
     name="oak-lab",
-    version="1.1.2",
+    version="1.1.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
